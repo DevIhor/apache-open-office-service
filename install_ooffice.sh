998d11c369e127b11f3c8750a652729a42068096
Ihor <ihor.l@coaxsoft.com>
2022-12-19 17:21:00 +0200

    Create install_ooffice.sh

---
+++ install_ooffice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://sourceforge.net/projects/openofficeorg.mirror/files/4.1.12/binaries/en-US/Apache_OpenOffice_4.1.12_Linux_x86-64_install-deb_en-US.tar.gz
+tar -zxvf Apache_OpenOffice_4.1.12_Linux_x86-64_install-deb_en-US.tar.gz
+cd en-US/DEBS/
+sudo dpkg -i *.deb
+cd desktop-integration
+sudo dpkg -i *.deb
